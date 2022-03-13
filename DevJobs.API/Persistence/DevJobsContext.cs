5e8b51f3ffcd36e3c602d6031e2465857f5c6df3
Jorge Edson <jorge.edson.bsi@gmail.com>
2022-03-13 02:03:53 -0300

    Add project files.

---
+++ DevJobs.API/Persistence/DevJobsContext.cs
@@ -0,0 +1,35 @@
+﻿using DevJobs.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevJobs.API.Persistence
+{
+    public class DevJobsContext : DbContext
+    {
+        public DevJobsContext(DbContextOptions<DevJobsContext> context) : base(context)
+        {
+
+        }
+
+        public DbSet<JobVacancy> JobVacancies { get; set; }
+        public DbSet<JobApplication> JobApplications { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<JobVacancy>(e => {
+                e.HasKey(jv => jv.Id);
+
+                e.HasMany(jv => jv.Applications)
+                    .WithOne()
+                    .HasForeignKey(ja => ja.IdJobVacancy)
+                    .OnDelete(DeleteBehavior.Restrict);
+            });
+
+            modelBuilder.Entity<JobApplication>(e => {
+                e.HasKey(ja => ja.Id);
+            });
+        }
+    }
+}
